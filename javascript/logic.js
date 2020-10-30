87e8f78e1f343c3c3059c6cb3b4c35808e9b0e1b
Daniela Gutiérrez <dangtz14@gmail.com>
2020-10-29 18:48:51 -0600

    Final

--- javascript/logic.js
+++ javascript/logic.js
@@ -4,8 +4,9 @@ const ctx = canvas.getContext("2d");
 // VARIABLES
 let apples = [];
 let frames = 0;
-let score = 0;
-
+let points = 0;
+let requestID;
+let isWin = false;
 canvas.width = 1170;
 canvas.height = 700;
 document.body.appendChild(canvas);
@@ -35,25 +36,41 @@ class Background {
     }
 }
 
-// LUIGI IMAGE
+// WON / LOST
 
-const luigiImg = {
-
+class Status {
+    constructor (width, height, x, y, img) {
+    this.width = width;
+    this.height = height;
+    this.img = new Image ();
+    this.img.src = img;
+    this.x = x;
+    this.y = y;
+}
+draw() {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+}
 }
 
 // LUIGI
 
 class Luigi {
     constructor (width, height) {
-        this.x = 100;
-        this.y = 100;
+        this.x = 400;
+        this.y = 300;
         this.width = width;
         this.height = height;
         this.form = new Image();
-        this.form.src = "../images/LUIGI.png" 
+        this.form.src = "../images/LUIGI.png"
+        this.speed = 2;
     }
     draw() {
         ctx.drawImage(this.form, this.x, this.y, this.width, this.height)
+    }
+    collision(enemy) {
+        return (this.x < enemy.x + enemy.width && 
+            this.x + this.width > enemy.x && this.y < enemy.y + enemy.height && 
+            this.y + this.height > enemy.y)
     }
 }
 
@@ -75,46 +92,67 @@ class Enemies {
 }
 
 // CONTROLS
-let update = function (modifier) {
-	if (38 in keysDown) {
-		luigi.y -= luigi.speed * modifier;
-	}
-	if (40 in keysDown) {
-		luigi.y += luigi.speed * modifier;
-	}
-	if (37 in keysDown) {
-		luigi.x -= luigi.speed * modifier;
-	}
-	if (39 in keysDown) {
-		luigi.x += luigi.speed * modifier;
-    }
+
+addEventListener("keydown", (e) => {
+    e.preventDefault();
+switch (e.keyCode) {
+    case 38:
+        if (luigi.y < 0) {
+            return false
+        }
+        luigi.y -= luigi.speed * 9;        
+        break;
+        case 40:
+            if (luigi.y + luigi.height > canvas.height) {
+                return false
+            }
+        luigi.y += luigi.speed * 9;
+        break;
+        case 37:
+            if (luigi.x < 0) {
+                return false
+            }
+        luigi.x -= luigi.speed * 9;
+        break;
+        case 39:
+            if (luigi.x + luigi.width > canvas.width) {
+                return false
+            }
+        luigi.x += luigi.speed * 9;
+        break;
+    default:
+        break;
 }
-
-// COLLISION (GAME OVER) // COMENTED DUE TO ERROR ON PAGE
-
-// if (
-//     luigi.x <= (apple.x + 32)
-//     && apple.x <= (luigi.x + 32)
-//     && luigi.y <= (apple.y + 32)
-//     && apple.y <= (luigi.y + 32)
-// ) {
-//     reset();
-// };
-
-// UPDATE GAME
+});
 
 const fondo = new Background ()
-const luigi = new Luigi ()
+const luigi = new Luigi (200, 200)
+const letreroPierdo = new Status (400, 400, 370, 290, "./../images/over.png")
 
 updateGame = () => {
     frames++
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    luigi.draw()
     fondo.draw()
+    luigi.draw()
     generateApples()
     drawApples()
-    //gameArea.score()
-    requestAnimationFrame(updateGame);
+    score()
+    if (points >= 100) {
+        isWin=true
+        requestID=undefined
+        //letreroGano.drwa
+    }
+   
+    if (!requestID) {
+            letreroPierdo.draw()
+        //gameOver();
+
+    }else{
+        requestID = requestAnimationFrame(updateGame);
+    }
+
+    
+
 }
 
 // FUNCIÓN PARA GENERAR MUCHAS MANZANAS
@@ -128,27 +166,39 @@ generateApples = () => {
 drawApples = () => {
     apples.forEach( (apple, index) => {
         if (apple.y > canvas.height) {
-            //score += 10
+            points += 10
             return apples.splice(index, 1)
+        }
+        if (luigi.collision(apple)) {
+            isWin=false
+            requestID=undefined
         }
         apple.draw()
     })
 }
 
-// SCORE
-// var gameArea = {
+gameOver = () => {
 
-//     score: function () {
-//         var points = Math.floor(this.frames / 5);
-//         ctx.fillStyle = "rgb(250, 250, 250)";
-//         ctx.font = "24px Helvetica";
-//         ctx.textAlign = "left";
-//         ctx.textBaseline = "top";
-//         ctx.fillText("Score: " + points, 8, 20);
-//         }
-//     };
-//drawScore();
+  
+    console.log(isWin ? "You won!" : "Game Over")
+    letrero.draw();
     
-   
+}
 
-updateGame();
+// SCORE
+
+    score = () => {
+        
+        ctx.fillStyle = "rgb(250, 250, 250)";
+        ctx.font = "24px Helvetica";
+        ctx.textAlign = "left";
+        ctx.textBaseline = "top";
+        ctx.fillText("Score: " + points, 8, 20);
+    }
+
+window.onload = () => {
+    document.getElementById("startButton").onclick = () => {
+        requestID = requestAnimationFrame(updateGame);
+
+    }
+}
